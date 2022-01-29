edeaa193a825c9e355ac739ee0f79f3a999b56f5
JorgeUpGrade <jorgevazquezmorenob@gmail.com>
2022-01-29 12:03:34 +0100

    Airbnb clone

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css'
+function Footer() {
+  return <div className='footer'>
+      <p>Jorge ha clonado Airbnb</p>
+      <p>Privay Terms</p>
+  </div>;
+}
+
+export default Footer 
